351c5e821b28a60d03f6a0237f5ae2baec0dd075
maddywestergaard <43056846+maddywestergaard@users.noreply.github.com>
2019-01-08 15:38:42 -0800

    Add files via upload

---
+++ lab7/randmain.mk
@@ -0,0 +1,6 @@
+randlibhw.so: randlibhw.c
+	$(CC) $(CFLAGS) randlibhw.c -fPIC -shared -o randlibhw.so
+randlibsw.so: randlibsw.c
+	$(CC) $(CFLAGS) randlibsw.c -fPIC -shared -o randlibsw.so
+randmain: randmain.o randcpuid.o
+	$(CC) $(CFLAGS) randmain.o randcpuid.o -ldl -Wl,-rpath=$PWD -o randmain
